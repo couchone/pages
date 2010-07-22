ff1dcea4bcc044cedf8a98c31d62e4a140deecad
Chris Anderson <jchris@apache.org>
2010-07-22 13:27:53 -0700

    better index page redirects

---
+++ _docs/index/markdown.md
@@ -0,0 +1,3 @@
+## Home Page
+
+This is a markdown wiki. Edit it and you will be so happy.
---
+++ shows/redirect.js
@@ -0,0 +1,4 @@
+function() {  
+  var redirect = require("vendor/couchapp/lib/redirect");
+  return redirect.permanent("/page/index");
+}
