770342947498a48cb1a00f1b3801618233cf74b9
Chris Anderson <jchris@apache.org>
2010-07-31 15:03:09 -0700

    remove extra log

--- evently/wiki/edit/selectors/textarea[name=markdown]/keyup.js
+++ evently/wiki/edit/selectors/textarea[name=markdown]/keyup.js
@@ -2,6 +2,5 @@ function() {
   var form = $(this).parents("form"), app = $$(this).app,
     f = form.serializeObject(),
     markdown = app.require("vendor/couchapp/lib/markdown");
-  $.log(f)
   $(".preview", form).html(markdown.encode(f.markdown));
 };
